2bc9177c3620cc92566f04cc8961b59606430a0b
Tiffany Medina <t.medina1023@gmail.com>
2020-10-05 18:04:25 -0400

    Initial Commit

---
+++ InstaDupe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstaDupe
+//
+//  Created by Tiffany Medina on 10/5/20.
+//  Copyright © 2020 tiffanymedina23. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
